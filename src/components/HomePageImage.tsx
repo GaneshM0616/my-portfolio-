d650d89936c9ad5847fdb43586e008d4f9258b54
GaneshM0616 <loneman549@gmail.com>
2024-08-21 22:27:06 +0530

    Fix ESLint errors and warnings

--- src/components/HomePageImage.tsx
+++ src/components/HomePageImage.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image';
 
 function HomePageImage() {
   return (
     <div>
-        <img 
+        <Image
         src=''
         alt='Dev-e-lo-per.jpg'
         className='w-full h-auto max-w-xs rounded-lg'
